f15b740a0dee21e4047f602d7da7745c3879d003
vaois_ping <851149343@qq.com>
2016-04-28 23:03:48 +0800

    success

---
+++ split.c
@@ -0,0 +1,47 @@
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <stdio.h>
+int main(int argc,char *argv[])
+{
+   // char block[1024];
+    char c; //+
+    int in, out,out1; 
+    int nread;
+    size_t len;
+    struct stat statbuf;
+    size_t avg;
+    in = open(argv[1], O_RDONLY);
+    stat(argv[1],&statbuf);
+    len=statbuf.st_size;
+    avg=len/2; 
+    printf("size %d\n",len);
+    out = open(argv[2], O_WRONLY|O_CREAT, S_IRUSR|S_IWUSR);
+out1 = open(argv[3], O_WRONLY|O_CREAT, S_IRUSR|S_IWUSR);   
+ printf("01\n");
+   // while((nread = read(in,block,sizeof(block))) > 0 && avg>0)
+   //    { write(out,block,nread);
+             // avg-=1024;
+      // }
+     while(read(in,&c,1) == 1)
+      {    if(avg>0)
+          {  write(out,&c,1);
+              avg--;
+	  }
+else
+{
+	write(out1,&c,1);
+
+}
+ // printf("avg %d",avg);
+   //  lseek(in,0,SEEK_CUR);
+    //out1 = open(argv[3], O_WRONLY|O_CREAT, S_IRUSR|S_IWUSR);
+  // printf("02\n");
+    //while((nread = read(in,block,sizeof(block))) > 0)
+      // write(out,block,nread);
+          }  
+    exit(0);
+}
+
